b07c1e495a608fbdeaf6e3e038460f8708f5425c
Dragan Pepic <d.pepic@hotmail.com>
2020-09-19 19:49:44 +0200

    Add project files.

---
+++ ChatBlazorLive/Server/Hubovi/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using ChatBlazorLive.Shared;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBlazorLive.Server.Hubovi
+{
+	public class ChatHub : Hub
+	{
+		public void PrimiPoruku(ChatPoruka poruka)
+		{
+			Console.WriteLine($"[{poruka.VremeSlanja}] {poruka.Korisnik}: {poruka.Sadrzaj}");
+			Clients.All.SendAsync("pp", poruka);
+		}
+	}
+}
---
+++ ChatBlazorLive/Shared/ChatPoruka.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatBlazorLive.Shared
+{
+	public class ChatPoruka
+	{
+		public string Korisnik { get; set; }
+		public string Sadrzaj { get; set; }
+		public DateTime VremeSlanja { get; set; }
+	}
+}
